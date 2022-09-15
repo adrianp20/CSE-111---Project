913f1b3494acd408e2a182e8ae65d76a96af54b4
TensorCore <shakerad0109@gmail.com>
2022-09-14 23:34:45 -0700

    Auth Inclusion

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 // @TS-nocheck
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
@@ -15,5 +16,19 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
