97e05362d88d16b2dc4769ff09e6520d32f73243
TensorCore <shakerad0109@gmail.com>
2022-09-15 09:42:43 -0700

    Remove Interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import type { NextPage } from 'next';
 import Component from '../components/login-btn';
-// interface Props {
-//   data: {
-//     name: string,
-//   };
-// }
 
 const Home: NextPage = () => (
   <div className="flex">
