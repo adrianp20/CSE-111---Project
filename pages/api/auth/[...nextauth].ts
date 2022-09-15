9b2420c39c519083168416453018bb12504783d6
TensorCore <shakerad0109@gmail.com>
2022-09-14 22:36:15 -0700

    Fixing Up Auth & Prisma Integration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,13 @@
 // @TS-nocheck
-
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT,
