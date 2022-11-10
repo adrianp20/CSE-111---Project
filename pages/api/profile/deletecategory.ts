1504b7ac7e5060c99ecc68d6055820f0064ffc6a
TensorCore <shakerad0109@gmail.com>
2022-11-10 11:37:07 -0800

    New queries

---
+++ pages/api/profile/deletecategory.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+// import { unstable_getServerSession } from 'next-auth/next';
+// import { authOptions } from '../auth/[...nextauth]';
+
+import { prisma } from '../../../lib/prisma';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // const session = await unstable_getServerSession(req, res, authOptions);
+  // if (session) {
+  const data = req.body;
+  const lobby = await prisma.profile.update({
+    where: {
+      userId: data.userId,
+    },
+    data: {
+      category: {
+        disconnect: data.categoryIds.map((id: any) => ({ id })),
+      },
+    },
+  });
+
+  res.status(200).json({ lobby });
+  // } else {
+  //   res.status(401).json({
+  //     error: 'You must be signed in',
+  //   });
+  // }
+}
